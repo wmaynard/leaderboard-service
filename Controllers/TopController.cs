cb14662cc674aa9114db1cfe54da70f621a5948f
Will Maynard <will@willmaynard.com>
2022-06-14 16:23:31 -0700

    Formatting TopController

--- Controllers/TopController.cs
+++ Controllers/TopController.cs
@@ -6,66 +6,65 @@ using Rumble.Platform.LeaderboardService.Exceptions;
 using Rumble.Platform.LeaderboardService.Models;
 using Rumble.Platform.LeaderboardService.Services;
 
-namespace Rumble.Platform.LeaderboardService.Controllers
+namespace Rumble.Platform.LeaderboardService.Controllers;
+
+[ApiController, Route("leaderboard"), RequireAuth, UseMongoTransaction]
+public class TopController : PlatformController
 {
-	[ApiController, Route("leaderboard"), RequireAuth, UseMongoTransaction]
-	public class TopController : PlatformController
-	{
 #pragma warning disable
-		private Services.LeaderboardService _leaderboardService;
-		private EnrollmentService _enrollmentService;
-		private ResetService _resetService;
-		private RewardsService _rewardsService;
-		private DynamicConfigService _config;
+	private Services.LeaderboardService _leaderboardService;
+	private EnrollmentService _enrollmentService;
+	private ResetService _resetService;
+	private RewardsService _rewardsService;
+	private DynamicConfigService _config;
 #pragma warning restore
 
-		[HttpPatch, Route("score")]
-		public ActionResult AddScore()
-		{
-			int score = Require<int>("score");
-			string type = Require<string>(Leaderboard.FRIENDLY_KEY_TYPE);
+	[HttpPatch, Route("score")]
+	public ActionResult AddScore()
+	{
+		int score = Require<int>("score");
+		string type = Require<string>(Leaderboard.FRIENDLY_KEY_TYPE);
 
-			if (score == 0)
-				return Ok();
+		if (score == 0)
+			return Ok();
 
-			Enrollment enrollment = _enrollmentService.FindOrCreate(Token.AccountId, type);
-			Leaderboard leaderboard = _leaderboardService.AddScore(enrollment, score);
-			_rewardsService.Validate(Token.AccountId);
+		Enrollment enrollment = _enrollmentService.FindOrCreate(Token.AccountId, type);
+		Leaderboard leaderboard = _leaderboardService.AddScore(enrollment, score);
+		_rewardsService.Validate(Token.AccountId);
 
-			if (leaderboard == null)
-				throw new UnknownLeaderboardException(type);
+		if (leaderboard == null)
+			throw new UnknownLeaderboardException(type);
 
-			if (enrollment.CurrentLeaderboardID == leaderboard.Id)
-				return Ok(new { Leaderboard = leaderboard });
-			
-			enrollment.CurrentLeaderboardID = leaderboard.Id;
-			enrollment.IsActive = true;
-			_enrollmentService.Update(enrollment);
-
+		if (enrollment.CurrentLeaderboardID == leaderboard.Id)
 			return Ok(new { Leaderboard = leaderboard });
-		}
+		
+		enrollment.CurrentLeaderboardID = leaderboard.Id;
+		enrollment.IsActive = true;
+		_enrollmentService.Update(enrollment);
 
-		// TODO: Move to admin controller
+		return Ok(new { Leaderboard = leaderboard });
+	}
+
+	// TODO: Move to admin controller
+	
+	
+	[HttpGet, Route("rankings")]
+	public ActionResult GetRankings()
+	{
+		string type = Require<string>(Leaderboard.FRIENDLY_KEY_TYPE);
 		
+		Enrollment enrollment = _enrollmentService.FindOrCreate(Token.AccountId, type);
+		Leaderboard leaderboard = _leaderboardService.AddScore(enrollment, 0);
+		// Leaderboard board = _leaderboardService.Find(Token.AccountId, type);
+		if (leaderboard == null)
+			throw new UnknownLeaderboardException(type);
 		
-		[HttpGet, Route("rankings")]
-		public ActionResult GetRankings()
+		return Ok( new
 		{
-			string type = Require<string>(Leaderboard.FRIENDLY_KEY_TYPE);
-			
-			Enrollment enrollment = _enrollmentService.FindOrCreate(Token.AccountId, type);
-			Leaderboard leaderboard = _leaderboardService.AddScore(enrollment, 0);
-			// Leaderboard board = _leaderboardService.Find(Token.AccountId, type);
-			if (leaderboard == null)
-				throw new UnknownLeaderboardException(type);
-			
-			return Ok( new
-			{
-				LeaderboardId = leaderboard.Type,
-				Tier = leaderboard.Tier,
-				SeasonalMaxTier = enrollment.SeasonalMaxTier,
-				Response = leaderboard.GenerateScoreResponse(Token.AccountId),
-			});
-		}
-}
+			LeaderboardId = leaderboard.Type,
+			Tier = leaderboard.Tier,
+			SeasonalMaxTier = enrollment.SeasonalMaxTier,
+			Response = leaderboard.GenerateScoreResponse(Token.AccountId),
+		});
+	}
 }
