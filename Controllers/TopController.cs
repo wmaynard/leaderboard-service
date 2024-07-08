f5e3f24b1c1b5ab1baa53989a980be06633bf9f0
Will Maynard <will@willmaynard.com>
2024-07-08 16:10:00 -0700

    Calculate scores for returned boards, correctly this time

--- Controllers/TopController.cs
+++ Controllers/TopController.cs
@@ -101,15 +101,15 @@ public class TopController : PlatformController
 				output.Add(shard.Copy());
 		}
 
-		foreach (Leaderboard board in output)
-			board?.CalculateRanks();
-
 		return !returnShards
-		? Ok()
-		: Ok(new RumbleJson
-		{
-			{ "leaderboards", output }
-		});
+			? Ok()
+			: Ok(new RumbleJson
+			{
+				{
+					"leaderboards", output
+						.Select(board => board.GenerateScoreResponse(Token.AccountId))
+				}
+			});
 	}
 	
 	[HttpGet, Route("rankings")]
